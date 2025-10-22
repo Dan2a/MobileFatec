c0a4b1c5d714248887cd87d20680e075ebf93344
Daniel <dan.andrade313@gmail.com>
2025-10-22 14:02:18 -0300

    ajustado controllers de sale

--- Primeira-API/src/controllers/sale/UpdateSalesController.ts
+++ Primeira-API/src/controllers/sale/UpdateSalesController.ts
@@ -4,19 +4,16 @@ import { UpdateSaleService } from "../../services/sale/UpdateSaleService";
 class UpdateSalesController {
     async handle(request: Request, response: Response) {
         const updateSaleService = new UpdateSaleService();
-
-        const id = request.params.id || "";
-        if (!id) {
-            throw Error("Dados incorretos");
-        }
-
+        const { id } = request.params;
         const { value, discount, productId, clientId } = request.body;
 
-        const sale = { id, value, discount, productId, clientId };
+        const sale = await updateSaleService.execute({ 
+            id, value, discount, 
+            productId: String(productId), 
+            clientId: String(clientId) 
+        });
 
-        const resp = await updateSaleService.execute(sale);
-
-        response.json({ message: `Venda com ID ${id} atualizada com sucesso`, data: resp });
+        return response.json(sale);
     }
 }
 export { UpdateSalesController };
